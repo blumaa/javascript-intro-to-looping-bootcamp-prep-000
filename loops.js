65032787ca1c8f25011bdf5c7b8746ecba5e0580
Aaron Blum <blumaa@gmail.com>
2019-07-17 16:43:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,7 +16,7 @@ function whileLoop(n){
   return ('done')
 }
 
-function doWhileLoop(i){
+function doWhileLoop(num){
   function incrementVariable() {
     i = i + 1;
     return i;
@@ -24,5 +24,5 @@ function doWhileLoop(i){
 
   do {
     console.log("I run once regardless.")
-  } while (incrementVariable(i) < 10)
+  } while (incrementVariable() < num)
 }
